df91e0f7cc4c446ce91956ff4dec13fd669ed5c4
Zain <zainulabideen2518@gmail.com>
2025-09-15 16:52:01 +0500

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,23 @@
 import express from "express";
 import { ENV } from "./config/env.js";
 import authRoutes from "./routes/authRoutes.js";
+import path from "path";
 
 const app = express();
+const _dirname = path.resolve();
 
 const PORT = ENV.PORT;
 
 app.use("/api/auth", authRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(_dirname, "../frontend/dist")));
+
+  // ✅ Catch-all route for React/Vue/SPA
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(_dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log("Server Started");
