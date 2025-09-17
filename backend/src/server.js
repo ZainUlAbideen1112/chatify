d924161ac3592262d87104886770a923114578f1
Zain <zainulabideen2518@gmail.com>
2025-09-17 20:01:09 +0500

    message routes and controllers added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { ENV } from "./config/env.js";
 import authRoutes from "./routes/authRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js";
 import path from "path";
 import { connectDb } from "./lib/db.js";
 import cookieParser from "cookie-parser";
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(_dirname, "../frontend/dist")));
